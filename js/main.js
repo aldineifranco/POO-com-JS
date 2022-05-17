9156a423c9be43b191abd03c868dd2dbf25d706e
Aldinei <93042673+aldineifranco@users.noreply.github.com>
2022-05-16 21:38:49 -0300

    adicionando produto
    vídeo 2, adicionando e manipulando objetos.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,57 @@
 class Produto {
 
     constructor() {
-        this.id = 0
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1
+        this.arrayProdutos = [];
     }
 
-    adicionar(){
-        alert('Produto adicionado');
+    salvar(){
+        let produto = this.lerDados();
+        this.validaCampo(produto)
+        console.log(produto);
+
+        if(this.validaCampo(produto)) {
+            this.adicionar(produto);
+        }
+
+        console.log(this.arrayProdutos);
 
     }
 
-    excluir(){
+    adicionar(produto){
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados(){
+        let produto = {}
+
+            produto.id = this.id;
+            produto.nomeProduto = document.querySelector("#produto").value;
+            produto.precoProduto = document.querySelector("#preco").value;
+
+        return produto;
+    }
+
+    validaCampo(produto){
+        let msg = '';
+
+        if(produto.nomeProduto === ''){
+            msg += '- Informe o nome do Produto \n';
+        }
+        if(produto.precoProduto === ''){
+            msg += '- Informe o preço do Produto \n';
+        }
+
+        if(msg != '') {
+            alert(msg);
+            return false;
+        }
+
+        return true;
+    }
+
+    cancelar(){
         alert('Produto excluído')
 
     }
